0cdc760398ff912a8b100031971049ae3160c4ef
aubhree <aubhree@gmail.com>
2015-03-12 21:27:58 -1000

    Check-in 1
    iCalendar main; createFile object. Includes making .ICS file with CLASS,
    TZID, DTSTART, DTEND, LOCATION, PRIORITY, SUMMARY taken from user. [NO
    ERROR CHECKING]

---
+++ iCalendar.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+
+//java.io;
+
+public class iCalendar
+{
+  
+    public static void main(String [] args) throws FileNotFoundException
+    {
+
+        createFile newFile = new createFile();
+        newFile.printToFile();
+        //changes made for update
+		
+    }
+}
