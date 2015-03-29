acd973ac7b400b2caf598e8f105ce28ae6788a5d
aubhree <aubhree@gmail.com>
2015-03-29 03:54:26 -1000

    Update iCalendar.java
    allows several event file creation

--- iCalendar.java
+++ iCalendar.java
@@ -1,16 +1,28 @@
 import java.io.FileNotFoundException;
+import javax.swing.JOptionPane;
 
-//java.io;
+/**
+* iCalendar.java
+*
+* @author TeamOrange
+*/
 
-public class iCalendar
-{
+public class iCalendar {
   
-    public static void main(String [] args) throws FileNotFoundException
-    {
+  public static void main(String [] args) throws FileNotFoundException {
+    
+    int numEvent = 1;
+    String uzone = "";
+    String newEvent = "yes";
 
-        createFile newFile = new createFile();
-        newFile.printToFile();
-        //changes made for update
-		
+    createFile newFile = new createFile();
+    
+    while (newEvent.equals("yes")) {
+      
+      numEvent = newFile.printToFile(numEvent, uzone);  
+      newEvent = JOptionPane.showInputDialog("Do you wish to enter in another event?: ");
+      newEvent = newEvent.toLowerCase();
     }
+  }
 }
+
